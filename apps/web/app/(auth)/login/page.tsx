6d251864eaeab7b6ae138b9a4cd781f2d318be9d
WiseyXD <aryan.s.nag@gmail.com>
2025-03-15 00:31:45 +0530

    build : landing page assets downloaded and signup page reformed

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -9,20 +9,5 @@ type Props = {
 };
 
 export default function SignIn({ searchParams }: Props) {
-  const resolvedSearchParams = use(searchParams);
-
-  return (
-    <AuthForm
-      mode="login"
-      title="Welcome Back"
-      description="Sign in to your account"
-      handleEmailAuth={handleSignInAndRedirect}
-      handleGoogleAuth={handleSignInAndRedirect}
-      searchParams={resolvedSearchParams}
-      alternateAuthLink={{
-        text: "Don't have an account? Sign up",
-        href: "/signup",
-      }}
-    />
-  );
+  return <AuthForm />;
 }
