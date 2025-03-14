370e2dc4aaa5337bbe51583754fdc1bd4bdff155
WiseyXD <aryan.s.nag@gmail.com>
2025-03-15 00:04:30 +0530

    build : working on landing

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image, { type ImageProps } from "next/image";
+import HeroSection from "@/components/landing/hero-section";
+import React from "react";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
+export default function page() {
   return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Hello World</h1>
-      <Button variant={"outline"}>Click me</Button>
-    </div>
+    <main>
+      <HeroSection />
+    </main>
   );
 }
